7936b1c072a87b1725fc5cc6ba6582850878906e
bipinsingh061 <43286371+bipinsingh061@users.noreply.github.com>
2020-09-06 11:26:29 +0530

    Create BadNeighbour Topcoder Problem.cpp
    Problem Statement : 
    The old song declares "Go ahead and hate your neighbour", and the residents of Bonneville have taken those words to heart. Every resident hates his next-door neighbours on both sides. Nobody is willing to live farther away from the town's well than his neighbours, so the town has been arranged in a big circle around the well. Unfortunately, the town's well is in disrepair and needs to be restored. You have been hired to collect donations for the Save Our Well fund.
    
    Each of the town's residents is willing to donate a certain amount, as specified in the int[] donations, which is listed in clockwise order around the well. However, nobody is willing to contribute to a fund to which his neighbour has also contributed. Next-door neighbours are always listed consecutively in donations, except that the first and last entries in donations are also for next-door neighbours. You must calculate and return the maximum amount of donations that can be collected.

---
+++ BadNeighbour Topcoder Problem.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+int solve(int ar[] , int s , int en)
+{
+    int mxd[en-s+1];
+    mxd[0]=ar[s];
+    mxd[1]=max(ar[s],ar[s+1]);
+
+    for(int i=2 ,k=s+2; i<en-s+1 ; ++i)
+    {
+        mxd[i]=max(ar[k++]+mxd[i-2] , mxd[i-1]);
+    }
+
+    return mxd[en-s];
+
+}
+
+int32_t main()
+{
+    inputoutput();
+    
+    int n;
+    cin>>n;
+    int ar[n];
+    char ch;
+    for(int i=0 ; i<n ; ++i)
+    {
+        cin>>ar[i];
+        cin>>ch ;
+
+    }
+
+    cout<<max(solve(ar,0,n-2),solve(ar,1,n-1));
+
+
+
+    return 0;
+}
